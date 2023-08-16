4af94836731ac787e4c09063301f0b79b569fd0f
GermanPachec0 <87890408+GermanPachec0@users.noreply.github.com>
2023-08-16 19:06:53 -0300

    add project struct

---
+++ app.js
@@ -0,0 +1,17 @@
+import express, { json } from 'express' // require -> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
+
+app.use('/product', moviesRouter)
+
+const PORT = 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
