8dd50131c2187320894ab0fdbb8b1a905694a034
AngelSony <angelfol@outlook.com>
2023-08-16 22:06:52 -0300

    BackEnd PaymentType DB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express' // require -> commonJS
 import categoryRouter  from './routes/category.js'
+import payment_typeRouter  from './routes/payment_type.js'
 //import { corsMiddleware } from './middlewares/cors.js'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import db from './config/database.js'
@@ -12,6 +13,7 @@ app.disable('x-powered-by')
 
 //app.use('/product', moviesRouter)
 app.use('/category', categoryRouter)
+app.use('/payment_type', payment_typeRouter)
 
 const PORT = 1234
 
