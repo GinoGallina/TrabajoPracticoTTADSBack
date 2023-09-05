d3c69e65c44d69d880965c4c5111e90f783adb8c
AngelSony <angelfol@outlook.com>
2023-09-05 18:43:08 -0300

    Payment type 80%

--- schemas/payment_type.ts
+++ schemas/payment_type.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod'
 
 const PaymentTypeSchema = z.object({
-  type: z.string().min(5)
+  type: z.string().min(5).refine(value => value.trim().length > 0, {
+    message: 'Type can not be empty ro contain only spaces'
+  }),
+  state: z.enum(['Active', 'Archived'], { message: 'Payment Type must be a valid type' }).default('Active')
 })
 
 export function validatePaymentType (input) {
