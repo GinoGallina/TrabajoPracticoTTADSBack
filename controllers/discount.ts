c15eb849678211832fd7b17fc53b2c39499963b4
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2023-10-19 16:05:47 -0300

    added discount repository

--- controllers/discount.ts
+++ controllers/discount.ts
@@ -1,85 +1,83 @@
-import Discount from '../models/database/discount.js'
-import { validateDiscount } from '../schemas/discount.js'
-import { Request, Response } from 'express';
+import { DiscountReposirory } from "../repository/discountRepository.js";
+import { validateDiscount } from "../schemas/discount.js";
+import { Request, Response } from "express";
 
+const discountReposirory = new DiscountReposirory();
 const discountController = {
-  getAllDiscounts: async (req: Request, res:Response) => {
+  getAllDiscounts: async (req: Request, res: Response) => {
     try {
-      const discounts = await Discount.find({ state: 'Active' }).populate('category')
-      res.status(200).json(discounts)
+      const discounts = await discountReposirory.findAll();
+      res.status(200).json(discounts);
     } catch (error) {
-      console.log(error)
-      res.status(500).json(error)
+      console.log(error);
+      res.status(500).json(error);
     }
   },
 
-  getDiscountById: async (req: Request, res:Response) => {
+  getDiscountById: async (req: Request, res: Response) => {
     try {
-      const discount = await Discount.findOne({
-        _id: req.params.id,
-        state: 'Active'
-      }).populate('category')
+      const id = req.params.id;
+      const discount = await discountReposirory.findOne({ id });
       if (!discount) {
-        return res.status(404).json({ error: 'Discount not found' })
+        return res.status(404).json({ error: "Discount not found" });
       }
-
-      res.status(200).json(discount)
+      res.status(200).json(discount);
     } catch (error) {
-      res.status(500).json(JSON.stringify(error))
+      res.status(500).json(JSON.stringify(error));
     }
   },
 
-  createDiscount: async (req: Request, res:Response) => {
+  createDiscount: async (req: Request, res: Response) => {
     try {
-      const result = validateDiscount(req.body)
+      const result = validateDiscount(req.body);
       if (!result.success) {
         // 422 Unprocessable Entity
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
+        return res
+          .status(400)
+          .json({ error: JSON.parse(result.error.message) });
       }
 
-      const newDiscount = new Discount(req.body)
-      const savedDiscount = await newDiscount.save()
-      res.status(201).json({ message: 'Discount created', data: savedDiscount })
+      const savedDiscount = await discountReposirory.add(req.body);
+      res
+        .status(201)
+        .json({ message: "Discount created", data: savedDiscount });
     } catch (error) {
-      res.status(500).json((error))
+      res.status(500).json(error);
     }
   },
 
-  updateDiscountById: async (req: Request, res:Response) => {
+  updateDiscountById: async (req: Request, res: Response) => {
     try {
-      const updatedDiscount = await Discount.findOneAndUpdate({
-        _id: req.params.id,
-        state: 'Active'
-      },
-      req.body,
-      { new: true })
+      const updatedDiscount = await discountReposirory.update(
+        req.params.id,
+        req.body
+      );
 
       if (!updatedDiscount) {
-        return res.status(404).json({ error: 'User not found' })
+        return res.status(404).json({ error: "User not found" });
       }
-      console.log(updatedDiscount)
-      res.status(200).json(updatedDiscount)
+      console.log(updatedDiscount);
+      res.status(200).json(updatedDiscount);
     } catch (error) {
-      res.status(500).json(error)
+      res.status(500).json(error);
     }
   },
-  deleteDiscountById: async (req: Request, res:Response) => {
+  deleteDiscountById: async (req: Request, res: Response) => {
     try {
-      const DiscountDeleted = await Discount.findByIdAndUpdate(
-        { _id: req.params.id },
-        { state: 'Archived' },
-        { new: true })
+      const id = req.params.id;
+      const DiscountDeleted = await discountReposirory.delete({ id });
 
       if (!DiscountDeleted) {
-        return res.status(404).json({ error: 'Discount not found' })
+        return res.status(404).json({ error: "Discount not found" });
       }
 
-      res.status(200).json({ message: 'Discount deleted', data: DiscountDeleted })
+      res
+        .status(200)
+        .json({ message: "Discount deleted", data: DiscountDeleted });
     } catch (error) {
-      res.status(500).json(error)
+      res.status(500).json(error);
     }
-  }
+  },
+};
 
-}
-
-export default discountController
+export default discountController;
