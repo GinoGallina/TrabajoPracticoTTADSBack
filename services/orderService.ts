9fd14a814f82d91ced061a94e03274419290d12f
AngelSony <angelfol@outlook.com>
2023-11-02 21:16:34 -0300

    Order Service 20%

--- services/orderService.ts
+++ services/orderService.ts
@@ -1,2 +1,22 @@
 import { OrderRepository } from "../repository/orderRepository.js";
-import { UserRepository } from "../repository/userRepository.js";
+
+const orderRepository = new OrderRepository();
+
+type ServiceResult<T> = {
+  success: boolean;
+  data?: T;
+  message?: string;
+}
+
+export const OrderService = {
+  /*create: async (params: IOrder): Promise<ServiceResult<IOrder | void>> => {
+
+    //Stock del Producto
+    //Estado del producto
+    //Crear Shipment
+    //Setear Unit-prize del producto
+    //Calcular amount
+    //
+
+  }*/
+}
