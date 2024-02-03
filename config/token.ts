c62e0d5df0d1e93667788be4aa37d15fd7c470a0
germanpachec0 <germanpacheco98@gmail.com>
2024-02-02 22:02:54 -0300

    validate token with public key in auth0

--- config/token.ts
+++ config/token.ts
@@ -1,16 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 import jwt, { Secret } from "jsonwebtoken";
-
+import fs from "fs";
 import { IAuthOUser } from "../types/Auth0Token";
 class TokenManager {
   private secretKey: Secret;
-
+  private publicKeyFilePath: string = "public-key.key";
+  private authkeys: string;
   constructor() {
     if (!process.env.SECRET_KEY) {
       throw new Error("Secret key must be defined.");
     }
     this.secretKey = process.env.SECRET_KEY;
+    this.authkeys = fs.readFileSync(this.publicKeyFilePath, "utf8");
   }
 
   generateToken(payload: Record<string, any>, expiresIn: string): string {
@@ -27,9 +29,15 @@ class TokenManager {
     }
   }
 
-  decodeAuth0Token(token: string): IAuthOUser | null {
-    const user: IAuthOUser | null = jwt.decode(token) as IAuthOUser;
-    return user;
+  decodeAuth0Token(token: string): IAuthOUser | any {
+    try {
+      const decodedData = jwt.verify(token, this.authkeys, {
+        algorithms: ["RS256"],
+      }) as IAuthOUser;
+      return decodedData;
+    } catch (error) {
+      return;
+    }
   }
 }
 
