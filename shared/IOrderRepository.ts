529f4017c91adb021b5519617054e54fa7c61dda
AngelSony <angelfol@outlook.com>
2025-03-13 18:05:34 -0300

    Revert "fixed things"
    This reverts commit dbb2c70446a8cd10b693900a2876acd1f952b5ae.

--- shared/IOrderRepository.ts
+++ shared/IOrderRepository.ts
@@ -4,6 +4,6 @@ export interface IOrderRepository<IOrder> {
 
   add(item: IOrder): Promise<IOrder | undefined>;
   update(id: string, item: IOrder): Promise<IOrder| undefined>;
-  delete(item: { id: string }): Promise<string | undefined>;
+  delete(item: { id: string }): Promise<IOrder | undefined>;
   updateAll(cartId: string, state: string, session: ClientSession): Promise<boolean | undefined>;
 }
